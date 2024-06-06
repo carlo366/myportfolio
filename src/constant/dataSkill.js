b6571f45b0e05bdf9b80bd406c7c8560efda4026
carlo366 <carloka366@gmail.com>
2024-06-06 14:52:28 +0700

    add: initial files

--- src/constant/dataSkill.js
+++ src/constant/dataSkill.js
@@ -2,31 +2,52 @@
 export default [
     {
         id : 1,
-        title : "HTML, CSS & Javascript",
-        content : "Membuat applikasi dengan native HTML CSS dan Javascipt.",
-        image : "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        title : "PHP Laravel , MSQL , JavaScript",
+        content : "Rancang dan Bangun Website Eccommerce Toko Teo Prabot",
+        image : "../src/assets/teo1.jpg",
         link : "/projek/html"
     },
     {
         id : 2,
-        title : "React JS / Frontend",
-        content : "Membuat applikasi single app dengan React JS dan Tailwind CSS",
-        image : "https://images.pexels.com/photos/11035471/pexels-photo-11035471.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        link : "/projek/react"
+        title : "PHP Laravel , MSQL , JavaScript",
+        content : "Rancang dan Bangun Website Eccomerce Toko Obat Wijaya Farma",
+        image : "../src/assets/wijaya.jpeg",
+        link : "https://github.com/carlo366/Pa1"
     },
     {
         id : 3,
-        title : "Express JS / Backend",
-        content : "Membuat Rest API server dengan Express JS",
-        image : "https://images.pexels.com/photos/11035380/pexels-photo-11035380.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        link : "/projek/express"
+        title : "Flutter , Dart , Laravel , MYSQL",
+        content : "Membuat Sistem Informasi DEL berbasis Mobil menggunakan Flutter Dat, Rest API dengan Menggunakan Laravel, dan MYSQL Managament Database ",
+        image : "../src/assets/mobile.jpg",
+        link : "https://github.com/carlo366/Aplikasi-Sistem-IT-DEL"
     },
     {
         id : 4,
-        title : "UI/UX Design",
-        content : "Design Prototype dengan applikasi Figma",
-        image : "https://images.pexels.com/photos/326502/pexels-photo-326502.jpeg?auto=compress&cs=tinysrgb&w=1600",
-        link : "/projek/design"
+        title : "Kotlin",
+        content : "Membuat User Interface Mobile Apps Jet Coffe dengan Kotlin",
+        image : "../src/assets/kotlin.jpg",
+        link : "https://github.com/carlo366/UserIinterfacemobileappscoffe"
     },
 
+    {
+        id : 5,
+        title : "Golang Microservice & Laravel Interface",
+        content : "Membuat Microservice RestFull Api Dengan Golang & Consume APi Laravel PHP ",
+        image : "../src/assets/teo1.jpg",
+        link : "https://github.com/carlo366/pasti_microservice"
+    },
+    {
+        id : 6,
+        title : "Java",
+        content : "Membuat aplikasi Booking Hotel berbasis Desktop menggunakan JAVA FX ",
+        image : "../src/assets/desktop.jpg",
+        link : "https://github.com/carlo366/Hotel"
+    },
+    {
+        id : 7,
+        title : "PHP Native",
+        content : "Membuat website Del Confes",
+        image : "../src/assets/delfes.png",
+        link : "https://youtu.be/frhXYEew3jY?si=fdGFFq-0JO89i4oV"
+    },
 ]

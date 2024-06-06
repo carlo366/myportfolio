05e95909a65cff6fa775afb1644d960ff0bb2158
carlo366 <carloka366@gmail.com>
2024-06-06 17:11:01 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Biodata from "./pages/Biodata";
 import DetailSkill from "./pages/DetailSkill";
 import Home from "./pages/Home";
 import Skill from "./pages/Skill";
-import Kemampuan from "./pages/Kemampuan"; // Import Kemampuan component
 
 export default function App() {
   return (
@@ -13,7 +12,6 @@ export default function App() {
       <Route path="/biodata" element={<Biodata />} />
       <Route path="/projek" element={<Skill />} />
       <Route path="/projek/:item" element={<DetailSkill />} />
-      <Route path="/skill" element={<Kemampuan />} /> {/* Add route for Kemampuan */}
     </Routes>
   );
 }
